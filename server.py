399e0bb8bf0de908583c8b083ba64c36603327f8
ClaireRuysschaert <netclaire@hotmail.fr>
2024-04-12 14:20:15 +0200

    feat: Add a message when secretary not enter email

--- server.py
+++ server.py
@@ -3,59 +3,81 @@ from flask import Flask, render_template, request, redirect, flash, url_for
 
 
 def loadClubs():
-    with open('clubs.json') as c:
-         listOfClubs = json.load(c)['clubs']
-         return listOfClubs
+    with open("clubs.json") as c:
+        listOfClubs = json.load(c)["clubs"]
+        return listOfClubs
 
 
 def loadCompetitions():
-    with open('competitions.json') as comps:
-         listOfCompetitions = json.load(comps)['competitions']
-         return listOfCompetitions
+    with open("competitions.json") as comps:
+        listOfCompetitions = json.load(comps)["competitions"]
+        return listOfCompetitions
 
 
 app = Flask(__name__)
-app.secret_key = 'something_special'
+app.secret_key = "something_special"
 
 competitions = loadCompetitions()
 clubs = loadClubs()
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/showSummary',methods=['POST'])
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/showSummary", methods=["POST"])
 def showSummary():
     try:
-        club = [club for club in clubs if club['email'] == request.form['email']][0]
+        form_email: str = request.form["email"]
+        club = [club for club in clubs if club["email"] == form_email][0]
     except IndexError:
-        return render_template('index.html', message="Sorry, we don't have your email address in our records. Please try again or contact the club."), 404
-    return render_template('welcome.html', club=club,competitions=competitions)
+        if form_email == "":
+            return (
+                render_template(
+                    "index.html", message="Please enter your email address"
+                ),
+                404,
+            )
+        else:
+            return (
+                render_template(
+                    "index.html",
+                    message="Sorry, we don't have your email address in our records. Please try again or contact the club.",
+                ),
+                404,
+            )
+    return render_template("welcome.html", club=club, competitions=competitions)
 
-@app.route('/book/<competition>/<club>')
-def book(competition,club):
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
+
+@app.route("/book/<competition>/<club>")
+def book(competition, club):
+    foundClub = [c for c in clubs if c["name"] == club][0]
+    foundCompetition = [c for c in competitions if c["name"] == competition][0]
     if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+        return render_template(
+            "booking.html", club=foundClub, competition=foundCompetition
+        )
     else:
         flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return render_template("welcome.html", club=club, competitions=competitions)
 
 
-@app.route('/purchasePlaces',methods=['POST'])
+@app.route("/purchasePlaces", methods=["POST"])
 def purchasePlaces():
-    competition = [c for c in competitions if c['name'] == request.form['competition']][0]
-    club = [c for c in clubs if c['name'] == request.form['club']][0]
-    placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    flash('Great-booking complete!')
-    return render_template('welcome.html', club=club, competitions=competitions)
+    competition = [c for c in competitions if c["name"] == request.form["competition"]][
+        0
+    ]
+    club = [c for c in clubs if c["name"] == request.form["club"]][0]
+    placesRequired = int(request.form["places"])
+    competition["numberOfPlaces"] = int(competition["numberOfPlaces"]) - placesRequired
+    flash("Great-booking complete!")
+    return render_template("welcome.html", club=club, competitions=competitions)
 
 
 # TODO: Add route for points display
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
